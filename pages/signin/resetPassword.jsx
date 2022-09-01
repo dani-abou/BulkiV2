fb6bde697ea2b610c886a96b9d7b501db7901baa
dani-abou <abouhamad.d@northeastern.edu>
2022-09-01 12:42:51 -0400

    testing deployment

--- pages/signin/resetPassword.jsx
+++ pages/signin/resetPassword.jsx
@@ -1,23 +1,23 @@
 import { ResetPassword } from "../../src/page_components"
 import { useRouter } from "next/router";
-import { confirmResetCode } from "../../src/assets/authentication"
+// import { confirmResetCode } from "../../src/assets/authentication"
 
-const ResetPasswordPage = ({ headControl, validationResponse, ...props }) => {
+const ResetPasswordPage = ({ headControl, validationResponse = { type: 'success', email: 'dummy@gmail.com' }, ...props }) => {
 
   const router = useRouter()
   const { mode, oobCode } = router.query;
   return <ResetPassword validationResponse={validationResponse} />
 }
 
-export const getServerSideProps = async ({ query }) => {
+// export const getServerSideProps = async ({ query }) => {
 
-  const { oobCode, mode } = query;
-  const response = await confirmResetCode(oobCode, mode);
-  return {
-    props: {
-      validationResponse: response,
-    }
-  };
-}
+//   const { oobCode, mode } = query;
+//   const response = await confirmResetCode(oobCode, mode);
+//   return {
+//     props: {
+//       validationResponse: response,
+//     }
+//   };
+// }
 
 export default ResetPasswordPage
