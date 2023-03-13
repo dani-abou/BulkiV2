a1f13e29c3006fc1ecc559a34a52689de3c1f36c
dani-abou <abouhamad.d@northeastern.edu>
2023-03-12 23:32:12 -0400

    build

--- pages/api/mailer/templates.js
+++ pages/api/mailer/templates.js
@@ -1,12 +1,12 @@
-import { Html } from '@react-email/html';
-import { mailer } from './mailer';
+// import { Html } from '@react-email/html';
+// import { mailer } from './mailer';
 
-export async function sendOrderConfirmEmail(recipients) {
-  const subject = "Your order is confirmed"
-  const html = () => {
-    return <Html lang="en">
-      Test
-    </Html>
-  }
-  await mailer(recipients, subject, html)
-}
+// export async function sendOrderConfirmEmail(recipients) {
+//   const subject = "Your order is confirmed"
+//   const html = () => {
+//     return <Html lang="en">
+//       Test
+//     </Html>
+//   }
+//   await mailer(recipients, subject, html)
+// }
--- pages/api/payment/makepayment.js
+++ pages/api/payment/makepayment.js
@@ -1,37 +1,37 @@
-import stripe from "stripe";
-import config from "./config";
+// import stripe from "stripe";
+// import config from "./config";
 
-const ourStripe = stripe(process.env.NEXT_PUBLIC_STRIPE_SECRET_KEY)
+// const ourStripe = stripe(process.env.NEXT_PUBLIC_STRIPE_SECRET_KEY)
 
-export default async function makepaymentintent(req, res) {
-  try {
+// export default async function makepaymentintent(req, res) {
+//   try {
 
-    const { amount, destination } = req.body;
-    if (!amount) throw 'Invalid price'
+//     const { amount, destination } = req.body;
+//     if (!amount) throw 'Invalid price'
 
-    // Create a PaymentIntent with the order amount and currency
-    const paymentIntent = await ourStripe.paymentIntents.create({
-      amount: amount,
-      currency: "usd",
-      automatic_payment_methods: {
-        enabled: true,
-      },
-      transfer_data: {
-        amount: amount * ((100 - config.platformFee) / 100),
-        destination: destination
-      }
-    });
-    res.status(200).json({ clientSecret: paymentIntent.client_secret })
-  } catch (e) {
-    res.status(501).json(e);
-  }
-};
+//     // Create a PaymentIntent with the order amount and currency
+//     const paymentIntent = await ourStripe.paymentIntents.create({
+//       amount: amount,
+//       currency: "usd",
+//       automatic_payment_methods: {
+//         enabled: true,
+//       },
+//       transfer_data: {
+//         amount: amount * ((100 - config.platformFee) / 100),
+//         destination: destination
+//       }
+//     });
+//     res.status(200).json({ clientSecret: paymentIntent.client_secret })
+//   } catch (e) {
+//     res.status(501).json(e);
+//   }
+// };
 
-// const paymentIntent = await stripe.paymentIntents.create({
-//   amount: 1000,
-//   currency: 'usd',
-//   transfer_data: {
-//     amount: 877,
-//     destination: '{{CONNECTED_STRIPE_ACCOUNT_ID}}',
-//   },
-// });
+// // const paymentIntent = await stripe.paymentIntents.create({
+// //   amount: 1000,
+// //   currency: 'usd',
+// //   transfer_data: {
+// //     amount: 877,
+// //     destination: '{{CONNECTED_STRIPE_ACCOUNT_ID}}',
+// //   },
+// // });
