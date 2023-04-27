f44f0860c065cda92ca5d12d85bd0423855887a4
dani-abou <abouhamad.d@northeastern.edu>
2023-04-27 16:22:45 -0400

    built

--- pages/legacy/catalog.jsx
+++ pages/legacy/catalog.jsx
@@ -1,21 +1,21 @@
-import { Catalog } from "../src/page_components";
-import { useEffect } from "react"
-import { useRouter } from "next/router";
-import useTranslation from 'next-translate/useTranslation'
-import { BulkiContextConsumer } from "../src/common/context";
+// import { Catalog } from "../src/page_components";
+// import { useEffect } from "react"
+// import { useRouter } from "next/router";
+// import useTranslation from 'next-translate/useTranslation'
+// import { BulkiContextConsumer } from "../src/common/context";
 
-const PAGE_META = {
-  title: 'Listings'
-}
+// const PAGE_META = {
+//   title: 'Listings'
+// }
 
-const ListingsPage = ({ headControls, ...props }) => {
-  useEffect(() => headControls(PAGE_META), [headControls])
+// const ListingsPage = ({ headControls, ...props }) => {
+//   useEffect(() => headControls(PAGE_META), [headControls])
 
-  const router = useRouter()
-  const { search } = router.query;
+//   const router = useRouter()
+//   const { search } = router.query;
 
-  return <Catalog searchQuery={search} />
-}
+//   return <Catalog searchQuery={search} />
+// }
 
 
-export default ListingsPage
+// export default ListingsPage
