c35c0983c5950db22350b4461e9c9fd48f181e32
dani-abou <abouhamad.d@northeastern.edu>
2023-05-11 14:44:58 -0400

    debugging

--- src/api/payment/makePayment.js
+++ src/api/payment/makePayment.js
@@ -1,12 +1,13 @@
 export default async function makePayment(stripe, elements, callback, errorCallback) {
   try {
-    const { origin } = window.location
+
+    // const { origin } = window.location
     const { error } = await stripe.confirmPayment({
       //`Elements` instance that was used to create the Payment Element
       elements,
       redirect: "if_required",
       confirmParams: {
-        return_url: origin,
+        return_url: "https://bulki.us",
       },
     });
     if (error) throw error;
